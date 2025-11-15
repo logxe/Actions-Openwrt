0c29186f0f28c26cf9c893bd58493b8b28830ef4
logxe <hongzetong@gmail.com>
2025-11-15 21:49:19 +0800

    Update mosdns and v2ray-geodata packages
    Removed existing golang and v2ray-geodata packages, then cloned new versions from specified repositories.

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,10 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
