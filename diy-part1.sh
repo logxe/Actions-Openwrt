c3291bdd6f3fa099cd7a8455a15b2ce8693e7aa2
logxe <hongzetong@gmail.com>
2025-11-15 21:48:51 +0800

    Clean up diy-part1.sh by removing comments
    Removed commented lines and cleaned up package management commands.

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,12 +19,3 @@
 #echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5" >> "feeds.conf.default"
 
 #echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
-
-# mosdns
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-
-rm -rf feeds/packages/net/v2ray-geodata
-
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
